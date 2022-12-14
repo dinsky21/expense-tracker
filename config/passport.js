8d3673ebc0f8cd226b1f16e784132d5ac8f32b47
Dinsky21 <dinsky21@gmail.com>
2022-12-14 15:13:47 +0800

    feat: login, logout and register with passport-local

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ override: true })
+// require('dotenv').config({ override: true })
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,65 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
-// const Todo = require('../../models/todo')
+const expenseList = require('../../models/expense')
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true,
+  })
+)
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'email and password are required' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'password does not match' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    })
+  }
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      errors.push({ message: 'email already exists' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) => {
+        User.create({ name, email, password: hash })
+          .then(res.redirect('/'))
+          .catch((err) => console.log(err))
+      })
+  })
 })
 
 router.get('/new', (req, res) => {
@@ -15,6 +68,16 @@ router.get('/new', (req, res) => {
 
 router.get('/edit', (req, res) => {
   res.render('edit')
+})
+
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    req.flash('success_msg', 'You have logged out')
+    res.redirect('/users/login')
+  })
 })
 
 module.exports = router
