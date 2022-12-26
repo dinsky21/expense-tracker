86248939be1753f7a32f4bb5c896c6beccf26b1a
Dinsky21 <dinsky21@gmail.com>
2022-12-26 16:13:24 +0800

    feat: add filter by category

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+// Require handlebars and just-handlebars-helpers
+const Handlebars = require('handlebars')
+const H = require('just-handlebars-helpers')
+
+// Register just-handlebars-helpers with handlebars
+H.registerHelpers(Handlebars)
+
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ override: true })
+	require('dotenv').config({ override: true })
 }
 const routes = require('./routes')
 const usePassport = require('./config/passport')
@@ -17,11 +24,11 @@ const PORT = process.env.PORT
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
 )
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -31,16 +38,16 @@ usePassport(app)
 
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg')
-  res.locals.error = req.flash('error')
-  next()
+	res.locals.isAuthenticated = req.isAuthenticated()
+	res.locals.user = req.user
+	res.locals.success_msg = req.flash('success_msg')
+	res.locals.warning_msg = req.flash('warning_msg')
+	res.locals.error = req.flash('error')
+	next()
 })
 
 app.use(routes)
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
+	console.log(`App is running on http://localhost:${PORT}`)
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -21,32 +21,52 @@ router.get('/', async (req, res) => {
 		})
 	})
 
-	res.render('index', { expenses: parsedExpenseList, totalAmount })
-	// expenseList
-	// 	.find({ userId })
-	// 	.lean()
-	// 	.sort({ date: 'asc' }) // desc
-	// 	.then((expenses) => res.render('index', { expenses }))
-	// 	.catch((error) => console.error(error))
+	res.render('index', {
+		expenses: parsedExpenseList,
+		totalAmount,
+		categories: parsedCategoryList,
+	})
 })
 
 // search function 動態路由，在req.query(<form>才有)中擷取keyword，再搭配filter, includes的功能呈現搜尋結果
 router.get('/search', async (req, res) => {
 	const keyword = req.query.keyword.trim().toLowerCase()
 	const userId = req.user._id
-	const parsedList = await expenseList.find({ userId }).lean()
-	const filteredList = parsedList.filter(
+	const parsedExpenseList = await expenseList.find({ userId }).lean()
+	const filteredList = parsedExpenseList.filter(
 		(e) =>
 			e.name.toLowerCase().includes(keyword) ||
 			e.category.includes(keyword)
 	)
-	console.log(filteredList)
 
 	if (filteredList.length >= 1 || keyword === '') {
 		res.render('index', { expenses: filteredList })
 	} else {
 		res.render('no_results')
 	}
+})
+
+router.post('/filter', async (req, res) => {
+	const category = req.body.category
+	const userId = req.user._id
+	const parsedExpenseList = await expenseList
+		.find({ userId, category })
+		.lean()
+	const parsedCategoryList = await categoryList.find().lean()
+	let totalAmount = 0
+	parsedExpenseList.forEach((exp) => {
+		totalAmount += exp.cost
+		parsedCategoryList.forEach((cat) => {
+			if (exp.category === cat.name) {
+				return (exp.icon = cat.icon)
+			}
+		})
+	})
+	res.render('index', {
+		expenses: parsedExpenseList,
+		totalAmount,
+		categories: parsedCategoryList,
+	})
 })
 
 // 匯出路由器
